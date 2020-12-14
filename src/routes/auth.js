d5530c21874ed6d57e11bdcecbde8cc1de50f2f3
Khushboo171289 <58450205+Khushboo171289@users.noreply.github.com>
2020-12-13 19:15:04 -0800

    Initial commit

---
+++ src/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { signup, signin } = require('../controller/auth');
+const { isRequestValidated, validateSignupRequest, validateSigninRequest } = require('../validators/auth');
+
+
+router.post('/signup', validateSignupRequest,isRequestValidated, signup);
+router.post('/signin',validateSigninRequest, isRequestValidated, signin);
+//router.post('/signout',validateSigninRequest, isRequestValidated, signin);
+
+// router.post('/profile',requireSignin, (req, res)=>{
+//     res.status(200).json({user:'profile'})
+// });
+
+module.exports = router;
